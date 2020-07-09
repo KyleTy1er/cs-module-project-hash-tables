e035adf94a6ddb42ed432c6fe681ddd69e644958
Kyle Yates <53956594+KyleTy1er@users.noreply.github.com>
2020-07-09 15:28:06 -0700

    word count, no dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,11 +1,50 @@
+
+'''
+# No Duplicates
+
+Input: a string of words separated by spaces. Only the letters `a`-`z`
+are utilized.
+
+Output: the string in the same order, but with subsequent duplicate
+words removed.
+
+There must be no extra spaces at the end of your returned string.
+
+The solution must be `O(n)`.
+'''
+
+
+
 def no_dups(s):
-    # Your code here
+
+    huh = '""'
+
+    if len(s) > 0:
+
+        words = s.split(' ')
+        new_list = [None] * len(words)
+
+        for i in range(len(words)):
+            if words[i] not in new_list:
+                new_list[i] = words[i]
+
+        Not_none_values = filter(None.__ne__, new_list)
+        new_list = list(Not_none_values)
+
+        return new_list
+
+    else:
+
+        return huh
 
 
 
 if __name__ == "__main__":
-    print(no_dups(""))
-    print(no_dups("hello"))
-    print(no_dups("hello hello"))
-    print(no_dups("cats dogs fish cats dogs"))
-    print(no_dups("spam spam spam eggs spam sausage spam spam and spam"))
+    x = no_dups("")
+    print(x)
+    x = no_dups("hello")
+    print(x)
+    # print(no_dups("hello hello"))
+    # print(no_dups("cats dogs fish cats dogs"))
+    # print(no_dups("spam spam spam eggs spam sausage spam spam and spam"))
+    # expected out put = ("spam eggs sausage and")
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -8,7 +8,6 @@ def word_count(s):
     translation_table = dict.fromkeys(map(ord, '"{:;,.-+=/|[]}()*^&"'), None)
 
     s = s.translate(translation_table)
-    # print(f" this is the translate: {s}")
 
     if s:
 
@@ -31,9 +30,6 @@ def word_count(s):
 
         return results
 
-
-
-
     # count each word and return the count
     # remove special characters
     # convert string to lowercase
@@ -49,33 +45,17 @@ def word_count(s):
 
 if __name__ == "__main__":
 
+# this test not passing:
 
-    x = (word_count(""))
+    # x = (word_count(""))
+    #
+    # print(x)
+    #
+    # if x == {}:
+    #     print("success")
 
-    print(x)
 
-    if x == {}:
-        print("success")
     # print(word_count("Hello      hello"))
     # print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     # print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
 
-# #
-# self.assertTrue(x == {'hello': 2, 'my': 2, 'cat': 2, 'and': 1, "doesn't": 1, 'say': 1, 'back': 1}
-#                      {'hello': 2, 'my': 2, 'cat': 2, 'and': 1, "doesn't": 1, 'say': 1, 'back': 1}
-
-# this = 'Hello, my cat. And my cat doesn\'t say "hello" back.'
-#
-# print(this.strip())
-
-#
-# ignored_characters = ["'", "{", ":", ";" , "," , ".", "-", "+", "=", "/", "|", "[", "]" , "{", "}", "(", ")", "*", "^", "&","'"]
-#
-# def ignore_chars(x):
-#     if x in ignored_characters:
-#         return x.replace(x, '')
-#
-# string_ex = "these { strings are + here"
-#
-# for i in string_ex:
-#     print(checker(i))
